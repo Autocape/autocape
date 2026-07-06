ce130fd2fa885547189b6657541543aa578fd8f1
tanu <70314098+tanuthedev@users.noreply.github.com>
2026-07-06 21:44:53 +0200

    refractor(ui): move autocape.html from inline/internal css to (mostly) tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// This is required for Tailwind CSS IDE Integration and not used at all, ignore this file.
+// To apply custom CSS plugins and themes, refer to tailwind v4 documentation for CDN configuration.
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
